d56d7b196ea8e4f4a4ff473af90bc32661fd845b
Unknown <bartek.marynczak@op.pl>
2018-06-26 00:06:52 +0200

    Select Path Fix

--- Components/Menus/AnimeSelect.js
+++ Components/Menus/AnimeSelect.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = async () => {
-  let dir = fs.readdirSync(Root + "/Anime");
+  let dir = fs.readdirSync(cfgPath + "/Anime");
   dir = dir.filter(name => path.extname(name) == ".json");
 
   dir.unshift(new inquirerMenager.Separator());
@@ -22,7 +22,7 @@ module.exports = async () => {
     }
   ]);
 
-  const file = fs.readFileSync(Root + "/Anime/" + answares.anime);
+  const file = fs.readFileSync(cfgPath + "/Anime/" + answares.anime);
   const json = JSON.parse(file);
 
   return json;
